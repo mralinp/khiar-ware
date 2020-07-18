28c20819464dc706239b71947823a7c6590b7dad
Angel <alinaderiparizi@yahoo.com>
2020-07-18 15:21:53 +0430

    Update Radar.cpp

--- Source/Radar.cpp
+++ Source/Radar.cpp
@@ -6,7 +6,7 @@ Radar::Radar(MemoryManager* memoryManager):ScriptBot(memoryManager) {
 
 void Radar::refresh() {
 	if (this->enable) {
-		for (int i = 0; i < 65; i++) {
+		for (int i = 0; i < 32; i++) {
 			DWORD entity = getEntity(i);
 			if (entity != NULL && isDormant(entity) == false) {
 				setSpotted(entity);
